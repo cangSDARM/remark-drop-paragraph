93ed55f1773af4859093a8ca61b7961aaf5d77ac
Allen Lee <648384410li@gmail.com>
2023-03-28 18:49:21 +0800

    init repo

---
+++ README.md
@@ -0,0 +1,70 @@
+# @allen/remark-drop-paragraph
+
+A [remark](https://remark.js.org) plugin for dropping unnecessary paragraph nodes
+
+The idea behind this plugin: https://github.com/mdx-js/mdx/issues/1170#issuecomment-725622285
+
+## Installation
+
+**Not available yet**
+
+```sh
+npm install @allen/remark-drop-paragraph
+```
+
+## Options
+
+- `unwrapTags(paragraph's child per each)`<br/>
+  If you need to unwrap outer paragraph, use this
+  method to iterate through the paragraph's children nodes and unwrap when one returns true<br/>
+  default: `['div', 'aside', 'header', 'main', 'figure'].includes(node.type) || isJsxElement(node)`
+- `noIncludeTags(paragraph's parent)`<br/>
+  If you need to drop internal paragraph, use this method
+  to iterate through the nodes that may contain paragraph, and drop the paragraph when it returns
+  true<br/>
+  default: `['mdxBlockElement'].includes(node.type) || isJsxElement(node)`
+
+## Usage
+
+### Source
+
+```ts
+const remarkDropParagraph = require('@allen/remark-drop-paragraph');
+
+remark().use(remarkDropParagraph).process(`
+<JsxOut>
+
+paragraph1 in
+
+</JsxOut>
+
+paragraph2 out <JsxComponent />
+`);
+```
+
+### Yields
+#### Without this
+
+```html
+<JsxOut>
+<p>
+  paragraph1 in
+</p>
+</JsxOut>
+
+<p>paragraph2 out <JsxComponent /></p>
+```
+
+#### With this
+
+```html
+<JsxOut>
+paragraph1 in
+</JsxOut>
+
+paragraph2 out <JsxComponent />
+```
+
+### License
+
+[MIT](LICENSE.md) @ [Allen Lee](https://github.com/cangSDARM)
---
+++ index.ts
@@ -0,0 +1,66 @@
+import { visit, SKIP } from 'unist-util-visit';
+
+import type { Plugin } from 'unified';
+import type { Root, Content } from 'mdast';
+
+type Node = Content & { children?: Content };
+
+const isJsxElement = (node: Content) => node.type?.startsWith('mdxJsx');
+
+const splice = ([] as Content[]).splice;
+const some = ([] as any[]).some;
+const paragraph = 'paragraph';
+
+export const DefaultOption = {
+  unwrapTags: (node: Content) =>
+    ['div', 'aside', 'header', 'main', 'figure'].includes(node.type || '') || isJsxElement(node),
+  noIncludeTags: (node: Content) =>
+    ['mdxBlockElement'].includes(node.type || '') || isJsxElement(node),
+};
+
+export type RemarkDropParagraphOption = Partial<typeof DefaultOption>;
+
+// https://github.com/mdx-js/mdx/issues/1170#issuecomment-725622285
+const remarkDropParagraph: Plugin<[RemarkDropParagraphOption?], Root> = (
+  options = DefaultOption,
+) => {
+  const { unwrapTags = () => false, noIncludeTags = () => false } = options || {};
+
+  const isNeedCleanInnerParagraph = (node: Content) => {
+    return noIncludeTags(node);
+  };
+
+  const isNeedCleanOuterParagraph = (nodes: Content[]) => {
+    // not all inline
+    return some.apply(nodes, [
+      (child) => {
+        return unwrapTags(child.type || '');
+      },
+    ]);
+  };
+
+  function visitor(node: Node, index: number | null, parent: Node | undefined) {
+    // if there are children available keep diving into them
+    if (Array.isArray(node.children)) {
+      node.children.forEach(function (child) {
+        visit(child, visitor as any);
+      });
+    }
+
+    const isParagraph = node.type === paragraph;
+
+    if (parent && typeof index === 'number' && isParagraph) {
+      if (isNeedCleanInnerParagraph(parent) || isNeedCleanOuterParagraph(node.children || [])) {
+        splice.apply(parent.children, [index, 1, ...(node.children || [])]);
+
+        return [SKIP, index];
+      }
+    }
+  }
+
+  return (root: Root) => {
+    visit(root, visitor as any);
+  };
+};
+
+export default remarkDropParagraph;

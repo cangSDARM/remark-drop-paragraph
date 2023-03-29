0f1c1e79470b1b8a75d4067ad13066ce427574bd
Allen Lee <648384410li@gmail.com>
2023-03-29 15:04:41 +0800

    don't do unwrap for unrelated elements

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ type Node = Content & { children?: Content };
 const isJsxElement = (node: Content) => node.type?.startsWith('mdxJsx');
 
 const splice = ([] as Content[]).splice;
-const some = ([] as any[]).some;
 const paragraph = 'paragraph';
 
 export const DefaultOption = {
@@ -22,21 +21,49 @@ export type RemarkDropParagraphOption = Partial<typeof DefaultOption>;
 
 // https://github.com/mdx-js/mdx/issues/1170#issuecomment-725622285
 const remarkDropParagraph: Plugin<[RemarkDropParagraphOption?], Root> = (
-  options = DefaultOption,
+  options
 ) => {
-  const { unwrapTags = () => false, noIncludeTags = () => false } = options || {};
+  const { unwrapTags = () => false, noIncludeTags = () => false } =
+    Object.assign({}, DefaultOption, options);
 
   const isNeedCleanInnerParagraph = (node: Content) => {
     return noIncludeTags(node);
   };
 
-  const isNeedCleanOuterParagraph = (nodes: Content[]) => {
-    // not all inline
-    return some.apply(nodes, [
-      (child) => {
-        return unwrapTags(child);
-      },
-    ]);
+  const isNeedCleanOuterParagraph = (node: Content) => {
+    return unwrapTags(node);
+  };
+
+  const travelChildren = (nodes: Content[]) => {
+    let paragraphChildren: Content[] = [];
+    const wrap = () =>
+      ({
+        type: "paragraph",
+        children: paragraphChildren,
+        position: paragraphChildren[0]?.position,
+      } as Content);
+
+    let needClean = false;
+
+    return {
+      children: nodes.reduce((acc, cur, idx) => {
+        if (isNeedCleanOuterParagraph(cur)) {
+          needClean = true;
+          const ret = [...acc, wrap(), cur];
+          paragraphChildren = [];
+
+          return ret;
+        } else {
+          paragraphChildren.push(cur);
+        }
+        if (idx >= nodes.length - 1 && paragraphChildren.length > 0) {
+          return [...acc, wrap()];
+        } else {
+          return acc;
+        }
+      }, [] as Content[]),
+      needClean,
+    };
   };
 
   function visitor(node: Node, index: number | null, parent: Node | undefined) {
@@ -49,9 +76,16 @@ const remarkDropParagraph: Plugin<[RemarkDropParagraphOption?], Root> = (
 
     const isParagraph = node.type === paragraph;
 
-    if (parent && typeof index === 'number' && isParagraph) {
-      if (isNeedCleanInnerParagraph(parent) || isNeedCleanOuterParagraph(node.children || [])) {
-        splice.apply(parent.children, [index, 1, ...(node.children || [])]);
+    if (isParagraph && parent && typeof index === "number") {
+      if (isNeedCleanInnerParagraph(parent)) {
+        splice.apply(parent.children, [index, 1, node.children || []]);
+
+        return [SKIP, index];
+      }
+
+      const { needClean, children } = travelChildren(node.children || []);
+      if (needClean) {
+        splice.apply(parent.children, [index, 1, ...children]);
 
         return [SKIP, index];
       }

96359c1f7c352a2d34b3ed7fc1b1ad4b00a76188
Allen Lee <648384410li@gmail.com>
2023-03-29 17:03:21 +0800

    fix splice

--- index.ts
+++ index.ts
@@ -78,7 +78,7 @@ const remarkDropParagraph: Plugin<[RemarkDropParagraphOption?], Root> = (
 
     if (isParagraph && parent && typeof index === "number") {
       if (isNeedCleanInnerParagraph(parent)) {
-        splice.apply(parent.children, [index, 1, node.children || []]);
+        splice.apply(parent.children, [index, 1, ...(node.children || [])]);
 
         return [SKIP, index];
       }
